19f549b9cac13a7d934b3d87c71e61cecc60f77b
YtRen <rytubuntulinux@gmail.com>
2016-04-05 20:22:24 +0800

    Limit two result

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 UrlPrefix = "https://www.google.com"
 
 soup = BeautifulSoup(open("index.html"),"lxml")
-result = soup.find_all(title=True,href=re.compile("^/video"))
+result = soup.find_all(title=True,href=re.compile("^/video"), limit=2)
 
 # Extract `href` part from `result` list   
 UrlPart = [tag['href'] for tag in result]
